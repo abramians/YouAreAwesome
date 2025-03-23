8616ff741b928db6706904cdea2eeb6ee7ec2f3f
Armen Melik-Abramians <abramians2110@gmail.com>
2025-03-23 10:43:02 -0700

    Added another button, spacer and frames

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,20 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = "I Am A Programmer!"
+    @State private var message2 = "Awesome!"
+    
     var body: some View {
         
         VStack {
+            Spacer()
+                
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            Spacer()
+            
+            HStack {
+                
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
         
